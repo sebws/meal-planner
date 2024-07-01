b31b1a80a4ef66aa6d9a9ce8ba72f1d2d6c3a655
Sebastian Wesley-Smith <bassyws@gmail.com>
2024-07-01 17:48:43 +1000

    fix: use discord user id

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -37,10 +37,9 @@ declare module "next-auth" {
  */
 export const authOptions: NextAuthOptions = {
   callbacks: {
-    session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id;
-        // session.user.role = user.role; <-- put other properties on the session here
+    session({ session, token }) {
+      if (token?.picture?.includes("discord")) {
+        session.user.id = token.sub!;
       }
       return session;
     },
